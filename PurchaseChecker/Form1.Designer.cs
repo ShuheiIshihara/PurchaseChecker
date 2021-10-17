f78a5fc9335c0f8ff59d47eebb8c2e4458597809
sseina <moeshuhei@gmail.com>
2021-10-18 01:08:05 +0900

    チェック機能を実装

--- PurchaseChecker/Form1.Designer.cs
+++ PurchaseChecker/Form1.Designer.cs
@@ -28,13 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CsvFile = new System.Windows.Forms.OpenFileDialog();
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.LoadFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TargetShopInfo = new System.Windows.Forms.ListView();
+            this.BeginButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IntervalTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EndButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CsvFile
+            // 
+            this.CsvFile.FileName = "openFileDialog1";
+            // 
+            // FilePath
+            // 
+            this.FilePath.Font = new System.Drawing.Font("メイリオ", 13F);
+            this.FilePath.Location = new System.Drawing.Point(12, 90);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(860, 33);
+            this.FilePath.TabIndex = 0;
+            // 
+            // LoadFileButton
+            // 
+            this.LoadFileButton.Font = new System.Drawing.Font("メイリオ", 13F);
+            this.LoadFileButton.Location = new System.Drawing.Point(12, 12);
+            this.LoadFileButton.Name = "LoadFileButton";
+            this.LoadFileButton.Size = new System.Drawing.Size(174, 39);
+            this.LoadFileButton.TabIndex = 1;
+            this.LoadFileButton.Text = "ファイル選択";
+            this.LoadFileButton.UseVisualStyleBackColor = true;
+            this.LoadFileButton.Click += new System.EventHandler(this.LoadFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "選択されたCSVファイル";
+            // 
+            // TargetShopInfo
+            // 
+            this.TargetShopInfo.Font = new System.Drawing.Font("メイリオ", 10F);
+            this.TargetShopInfo.HideSelection = false;
+            this.TargetShopInfo.Location = new System.Drawing.Point(12, 225);
+            this.TargetShopInfo.Name = "TargetShopInfo";
+            this.TargetShopInfo.Size = new System.Drawing.Size(860, 314);
+            this.TargetShopInfo.TabIndex = 3;
+            this.TargetShopInfo.UseCompatibleStateImageBehavior = false;
+            // 
+            // BeginButton
+            // 
+            this.BeginButton.Font = new System.Drawing.Font("メイリオ", 13F);
+            this.BeginButton.Location = new System.Drawing.Point(192, 12);
+            this.BeginButton.Name = "BeginButton";
+            this.BeginButton.Size = new System.Drawing.Size(174, 39);
+            this.BeginButton.TabIndex = 4;
+            this.BeginButton.Text = "チェック開始";
+            this.BeginButton.UseVisualStyleBackColor = true;
+            this.BeginButton.Click += new System.EventHandler(this.BeginButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 10F);
+            this.label2.Location = new System.Drawing.Point(8, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "実行間隔";
+            // 
+            // IntervalTextBox
+            // 
+            this.IntervalTextBox.Font = new System.Drawing.Font("メイリオ", 13F);
+            this.IntervalTextBox.Location = new System.Drawing.Point(12, 170);
+            this.IntervalTextBox.Name = "IntervalTextBox";
+            this.IntervalTextBox.Size = new System.Drawing.Size(117, 33);
+            this.IntervalTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 10F);
+            this.label3.Location = new System.Drawing.Point(135, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "秒";
+            // 
+            // EndButton
+            // 
+            this.EndButton.Font = new System.Drawing.Font("メイリオ", 13F);
+            this.EndButton.Location = new System.Drawing.Point(372, 12);
+            this.EndButton.Name = "EndButton";
+            this.EndButton.Size = new System.Drawing.Size(174, 39);
+            this.EndButton.TabIndex = 8;
+            this.EndButton.Text = "チェック終了";
+            this.EndButton.UseVisualStyleBackColor = true;
+            this.EndButton.Click += new System.EventHandler(this.EndButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(884, 551);
+            this.Controls.Add(this.EndButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IntervalTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BeginButton);
+            this.Controls.Add(this.TargetShopInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoadFileButton);
+            this.Controls.Add(this.FilePath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog CsvFile;
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Button LoadFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView TargetShopInfo;
+        private System.Windows.Forms.Button BeginButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IntervalTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button EndButton;
     }
 }
 
--- PurchaseChecker/Form1.cs
+++ PurchaseChecker/Form1.cs
@@ -1,20 +1,139 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
 
 namespace PurchaseChecker
 {
     public partial class Form1 : Form
     {
+        System.Timers.Timer timer;
+
         public Form1()
         {
             InitializeComponent();
+
+            // サイズ固定
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            // インターバル(秒)
+            this.IntervalTextBox.Text = "300";
+
+            // リスト初期化
+            this.TargetShopInfo.FullRowSelect = true;
+            this.TargetShopInfo.GridLines = true;
+            this.TargetShopInfo.View = View.Details;
+
+            var columnTitle = new ColumnHeader() { Text = "Title", Width = 300 };
+            var columnUrl = new ColumnHeader() { Text = "URL", Width = 300 };
+            var columnTarget = new ColumnHeader() { Text = "Target", Width = 100 };
+            var columnResult = new ColumnHeader() { Text = "Result", Width = 150 };
+            this.TargetShopInfo.Columns.AddRange(new ColumnHeader[] { columnTitle, columnUrl, columnTarget, columnResult });
+        }
+
+        private void LoadFileButton_Click(object sender, EventArgs e)
+        {
+            this.CsvFile = new OpenFileDialog();
+            this.CsvFile.Filter = "CSVファイル(*.csv)|*.csv";
+
+            if (this.CsvFile.ShowDialog() == DialogResult.OK)
+            {
+                this.FilePath.Text = this.CsvFile.FileName;
+                LoadCsvFile(this.FilePath.Text);
+            }
+        }
+
+        private void LoadCsvFile(string filePath)
+        {
+            if (Path.GetExtension(filePath).Equals("csv"))
+            {
+                return;
+            }
+
+            this.TargetShopInfo.Items.Clear();
+            this.TargetShopInfo.CheckBoxes = true;
+
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var textData = reader.ReadLine();
+                    string[] data = textData.Split(',');
+                    string[] tmp = new string[4] { data[0], data[1], data[2], "待機中" };
+                    var item = new ListViewItem(tmp);
+                    if (Boolean.Parse(data[3]))
+                    {
+                        item.Checked = true;
+                    }
+                    this.TargetShopInfo.Items.Add(item);
+                }
+            }
+        }
+
+        private void BeginButton_Click(object sender, EventArgs e)
+        {
+            IsPurchase();
+
+            // ミリ秒に変換しつつ、タイマ設定
+            timer = new System.Timers.Timer(int.Parse(this.IntervalTextBox.Text) * 1000);
+
+            timer.Elapsed += (sender2, e2) =>
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new Action(this.IsPurchase));
+                }
+            };
+
+            timer.Start();
+
+        }
+
+        private void IsPurchase()
+        {
+            foreach (ListViewItem item in this.TargetShopInfo.Items)
+            {
+                if (item.Checked)
+                {
+                    string url = item.SubItems[1].Text;
+                    string keyword = item.SubItems[2].Text;
+
+                    WebRequest request = WebRequest.Create(url);
+                    Stream objStream = request.GetResponse().GetResponseStream();
+
+                    StreamReader objReader = new StreamReader(objStream);
+
+                    string line = "";
+                    while (line != null)
+                    {
+                        line = objReader.ReadLine();
+
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            if (line.Contains(keyword))
+                            {
+                                item.SubItems[3].Text = "販売中";
+                                break;
+                            }
+                            else
+                            {
+                                item.SubItems[3].Text = "販売停止";
+                            }
+
+                        }
+                    }
+                }
+                else
+                {
+                    item.SubItems[3].Text = "待機中";
+                }
+            }
+        }
+
+        private void EndButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer.Dispose();
         }
     }
 }
